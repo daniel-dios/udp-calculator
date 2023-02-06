8ad1d2652e819c2e45935803319014f22285772e
daniel-dios <daniel.schezdios@gmail.com>
2023-02-06 20:27:49 +0100

    failing reception

--- untitled/src/test/java/client/ClientTest.java
+++ untitled/src/test/java/client/ClientTest.java
@@ -6,6 +6,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -16,6 +17,7 @@ import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClientTest {
 
@@ -32,7 +34,47 @@ public class ClientTest {
                 .submit(() -> Client.sendRequest(x));
 
         final String receivedDataInServer = future.get(10, TimeUnit.SECONDS);
-        System.out.println(receivedDataInServer);
+        assertThat(receivedDataInServer)
+                .contains("0x255");
+    }
+
+    @Test
+    void shouldReceiveMessages() throws UnknownHostException {
+        final int port = new Random().nextInt(1000) + 8000;
+        final ClientParameters x = buildRequest(port, "0", "x", "255");
+
+        Executors.newFixedThreadPool(1)
+                .submit(() -> send(port, "65280"));
+
+        final Optional<BigInteger> bigInteger = Client.sendRequest(x);
+
+        assertThat(bigInteger).isPresent().get().isEqualTo(BigInteger.valueOf(65280));
+    }
+
+    private String send(final int port, final String s) {
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            System.out.println("Listening...");
+
+            final byte[] receiveData = new byte[7];
+            final DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            serverSocket.receive(receivePacket);
+
+            System.out.println("[UDP " + receivePacket.getAddress() + "]");
+
+            DatagramPacket sendPacket = new DatagramPacket(
+                    s.getBytes(),
+                    "65280".getBytes().length,
+                    receivePacket.getAddress(),
+                    receivePacket.getPort()
+            );
+            serverSocket.send(sendPacket);
+
+            return new String(receivePacket.getData());
+        } catch (IOException e) {
+            System.out.println("IO ERROR");
+            return "error";
+        }
     }
 
     private static ClientParameters buildRequest(final int port, final String first, final String operation, final String second) throws UnknownHostException {
