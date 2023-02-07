426da4b3b50f0212e0e46f99a9723d0988419f83
daniel-dios <daniel.schezdios@gmail.com>
2023-02-07 10:21:27 +0100

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,30 @@
 import client.Client;
 import client.ClientParameters;
-import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        final Optional<ClientParameters> maybeParams = ClientParameters.parse(args);
-        if (maybeParams.isEmpty()) {
-            System.out.println("Va sin tildes:");
-            System.out.println("El formato correcto es: udpcli <ip_address_servidor> <port_servidor> <numero> <simbolo> <numero>");
-            System.out.println("Los simbolos son para suma '+', resta '-', multiplicacion 'x', 'X' o '*' y division ':' o '/'");
-            System.out.println("Los numeros deben ser valores enteros entre 0 y 255");
-            System.out.println("Para la division el segundo numero no podra ser 0.");
-            return;
-        }
+        ClientParameters
+                .parse(args)
+                .ifPresentOrElse(
+                        Main::sendRequest,
+                        Main::printInstructions
+                );
+    }
 
-        final var bytes = Client.sendRequest(maybeParams.get());
+    private static void sendRequest(final ClientParameters params) {
+        Client
+                .sendRequest(params)
+                .ifPresentOrElse(
+                        it -> System.out.println("El valor recibido es: " + it),
+                        () -> System.out.println("No se recibe nada por el socket.")
+                );
+    }
 
-        if (bytes.isPresent()) {
-            System.out.println("El valor recibido es: " + bytes.get());
-        } else {
-            System.out.println("No se recibe nada por el socket.");
-        }
-
+    private static void printInstructions() {
+        System.out.println("Va sin tildes :( :");
+        System.out.println("El formato correcto es: udpcli <ip_address_servidor> <port_servidor> <numero> <simbolo> <numero>");
+        System.out.println("Los numeros deben ser valores enteros entre 0 y 255");
+        System.out.println("Los simbolos son: para suma,'+'; resta, '-'; multiplicacion 'x', 'X' o '*' y division ':' o '/'");
+        System.out.println("Para la division el segundo numero no podra ser 0.");
     }
 }
