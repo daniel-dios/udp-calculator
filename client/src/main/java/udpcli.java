2712ad4de5a48e03e7e9c0434ffae0348938ec5d
daniel-dios <daniel.schezdios@gmail.com>
2023-02-08 20:48:16 +0100

    added todo

--- client/src/main/java/udpcli.java
+++ client/src/main/java/udpcli.java
@@ -34,27 +34,33 @@ public class udpcli {
                 System.out.printf("No se recibe nada por el socket despues del timeout de %ds.%n", TIMEOUT.toSeconds());
                 break;
             case KO:
-                System.out.printf("El servidor ha respondido con KO reason %s.%n", result.answer());
+                System.out.printf("El servidor ha respondido con KO con reason: %s.%n", result.answer());
                 break;
         }
     }
 
+    // TODO find a nice way
     private static void printInstructions() {
+        System.out.println("El formato correcto es: ");
+        System.out.println("udpcli <ip_address_servidor> <port_servidor> <operacion>");
+        System.out.println("\t<ip_adress_servidor> debe ser la IP del servidor UDP.");
+        System.out.println("\t<port_servidor> debe ser el puerto del servidor UDP.");
+        System.out.println(
+                "\t<operacion> debe ser un entero entre 0 y 255, seguido (sin espacio) de un simbolo de operacion:"
+                        + "(suma, '+'; resta, '-'; multiplicacion 'x', y division ':' )"
+                        + " y de otro numero de entre 0 y 255 [menos para la division, que sera de entre 1 y 255]"
+        );
+        System.out.println();
+        System.out.println("Ejemplo:");
+        System.out.println("udpcli " + getHostAddress() + " 8081 1x2");
+        System.out.println("java udpcli " + getHostAddress() + " 8081 1x2");
+    }
+
+    private static String getHostAddress() {
         try {
-            System.out.println(
-                    "\nEl formato correcto es: " +
-                            "\nudpcli <ip_address_servidor> <port_servidor> <numero> <simbolo> <numero>" +
-                            "\n\t<ip_adress_servidor> debe ser la IP del servidor UDP." +
-                            "\n\t<port_servidor> debe ser el puerto del servidor UDP." +
-                            "\n\t<numero>> debe ser un entero entre 0 y 255. Para division no es valido 0 en el segundo numero." +
-                            "\n\t<simbolo> para suma, '+'; resta, '-'; multiplicacion 'x', 'X' o '*' y division ':' o '/'." +
-                            "\n" +
-                            "\nEjemplo:" +
-                            "\nudpcli " + InetAddress.getLocalHost().getHostAddress() + " 8081 1 x 2" +
-                            "\njava udpcli " + InetAddress.getLocalHost().getHostAddress() + " 8081 1 x 2"
-            );
+            return InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
-            // no op
+            return "127.0.0.1";
         }
     }
 }
